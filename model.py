5e7ef66f2f9223613a88a340b02c4d1c988b3444
santa-sukitoku <santanaruse@gmail.com>
2020-01-19 17:06:56 +0900

    add classes

--- model.py
+++ model.py
@@ -7,3 +7,34 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 import numpy as np
 import time
+
+classes = ["Abstract_Expressionism",
+           "Action_painting",
+           "Analytical_Cubism",
+           "Art_Nouveau",
+           "Baroque",
+           "Color_Field_Painting",
+           "Contemporary_Realism",
+           "Cubism",
+           "Early_Renaissance",
+           "Expressionism",
+           "Fauvism",
+           "High_Renaissance",
+           "Impressionism",
+           "Mannerism_Late_RenaissanceMinimalism",
+           "Naive_Art_Primitivism",
+           "New_Realism",
+           "Northern_Renaissance",
+           "Pointillism",
+           "Pop_Art",
+           "Post_Impressionism",
+           "Realism",
+           "Rococo",
+           "Romanticism",
+           "Symbolism",
+           "Synthetic_Cubism",
+           "Ukiyo_e"]
+
+nb_classes = len(classes)
+
+img_weight, img_height = 150, 150
