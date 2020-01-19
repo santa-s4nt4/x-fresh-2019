7b29aec92ab207d183ff08e57b8946a44a70a080
santa-sukitoku <santanaruse@gmail.com>
2020-01-19 16:29:50 +0900

    add a new file

---
+++ model.py
@@ -0,0 +1,9 @@
+import os
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential, Model
+from keras.layers import Input, Activation, Dropout, Flatten, Dense
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+import numpy as np
+import time
