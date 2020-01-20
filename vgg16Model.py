eff68e68afb5f17e40a641e1609a3c46900aa457
santa-sukitoku <santanaruse@gmail.com>
2020-01-20 22:57:48 +0900

    ライブラリのインポートとモデル構築

--- vgg16Model.py
+++ vgg16Model.py
@@ -4,3 +4,28 @@ from keras.applications.vgg16 import VGG16
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import SGD
 from keras.callbacks import CSVLogger
+
+n_categories = 26
+batch_size = 32
+train_dir = ''
+validation_dir = ''
+file_name = 'x-fresh'
+
+base_model = VGG16(weights='imagenet', include_top=False,
+                   input_tensor=Input(shape=(224, 224, 3)))
+
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+prediction = Dense(n_categories, activation='softmax')(x)
+model = Model(inputs=base_model.input, output=prediction)
+
+for layer in base_model.layer[:15]:
+    layer.trainable = False
+
+model.compile(optimizer=SGD(lr=0.0001, momentum=0.9),
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
