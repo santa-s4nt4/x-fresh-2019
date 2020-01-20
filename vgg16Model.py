91a15e1c3ce1d3683a5f0650eac3861ea380b416
santa-sukitoku <santanaruse@gmail.com>
2020-01-21 05:26:43 +0900

    validation_stepsとstep_per_epochを追加

--- vgg16Model.py
+++ vgg16Model.py
@@ -5,12 +5,14 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import SGD
 from keras.callbacks import CSVLogger
 
-n_categories = 26
+n_categories = 27
 batch_size = 32
 train_dir = 'dataset/train'
 validation_dir = 'dataset/validation'
 file_name = 'x-fresh'
 
+num_train = 6160
+num_validation = 3754
 
 '''
 base_model = VGG16(
@@ -29,9 +31,9 @@ x = base_model.output
 x = GlobalAveragePooling2D()(x)
 x = Dense(1024, activation='relu')(x)
 prediction = Dense(n_categories, activation='softmax')(x)
-model = Model(inputs=base_model.input, output=prediction)
+model = Model(inputs=base_model.input, outputs=prediction)
 
-for layer in base_model.layer[:15]:
+for layer in base_model.layers[:15]:
     layer.trainable = False
 
 model.compile(optimizer=SGD(lr=0.0001, momentum=0.9),
@@ -41,7 +43,7 @@ model.compile(optimizer=SGD(lr=0.0001, momentum=0.9),
 model.summary()
 
 train_datagen = ImageDataGenerator(
-    rescale=1.0 / 255,
+    rescale=1.0/255,
     shear_range=0.2,
     zoom_range=0.2,
     horizontal_flip=True
@@ -66,10 +68,11 @@ validation_generator = validation_datagen.flow_from_directory(
 )
 
 hist = model.fit_generator(train_generator,
+                           steps_per_epoch=num_train//batch_size,
                            epochs=200,
                            verbose=1,
                            validation_data=validation_generator,
-                           callbacks=[CSVLogger(file_name + '.csv')])
-
+                           validation_steps=num_validation//batch_size,
+                           callbacks=[CSVLogger(file_name+'.csv')])
 
 model.save(file_name+'.h5')
