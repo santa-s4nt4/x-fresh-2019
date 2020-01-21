ba3233d4c353d92fc0a6908ba47d28dac1ef1d38
santa-sukitoku <santanaruse@gmail.com>
2020-01-21 22:10:45 +0900

    file名を変更するコードを追加

---
+++ rename.py
@@ -0,0 +1,8 @@
+import os
+import glob
+
+files = glob.glob("dataset/All/*.jpg")
+for i, old_name in enumerate(files):
+    new_name = "dataset/All/{0:04d}.jpg".format(i + 1)
+    os.rename(old_name, new_name)
+    print(old_name + " → " + new_name)
