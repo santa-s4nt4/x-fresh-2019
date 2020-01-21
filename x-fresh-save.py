ab4d7c8e7ecb3653868304a1ca72b8096adc035f
santa-sukitoku <santanaruse@gmail.com>
2020-01-21 20:46:48 +0900

    annoyのモデルの構築

--- x-fresh-save.py
+++ x-fresh-save.py
@@ -3,3 +3,28 @@ from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input, VGG16
 from keras.models import Model
 from annoy import AnnoyIndex
+
+img_dir_path = 'dataset/wikiart/All'
+annoy_model_path = 'model/x-fresh.ann'
+annoy_dim = 4096
+
+base_model = VGG16(weights="imagenet")
+model = Model(inputs=base_model.input,
+              outputs=base_model.get_layer("fc2").output)
+
+annoy_model = AnnoyIndex(annoy_dim)
+
+for i in range(1, 1001):
+    img_path = img_dir_path + 'art' + str(i) + '.jpg'
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+    fc2_features = model.predict(x)
+
+    annoy_model.add_item(i, fc2_features[0])
+    print(img_path, 'saved')
+
+annoy_model.build(1000)
+annoy_model.save(annoy_model_path)
