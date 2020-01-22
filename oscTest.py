6c4f1e8cab46ec8334585d8e962b1482760851b2
santa-sukitoku <santanaruse@gmail.com>
2020-01-23 06:32:18 +0900

    OSCを受け取ったら別のMessageをOSCで送信する実装．

---
+++ oscTest.py
@@ -0,0 +1,30 @@
+# server
+from pythonosc import osc_server
+from pythonosc.dispatcher import Dispatcher
+
+# client
+from pythonosc import udp_client
+from pythonosc.osc_message_builder import OscMessageBuilder
+
+IP = '127.0.0.1'
+SERVER_PORT = 54410
+CLIENT_PORT = 54411
+
+client = udp_client.UDPClient(IP, CLIENT_PORT)
+
+
+def oscReceive(unused_addr, signal):
+    print('Receive Number: ' + signal)
+    msg = OscMessageBuilder(address='/')
+    msg.add_arg('is genius.')
+
+    m = msg.build()
+    client.send(m)
+
+
+dispatcher = Dispatcher()
+dispatcher.map('/signal', oscReceive)
+
+server = osc_server.ThreadingOSCUDPServer((IP, SERVER_PORT), dispatcher)
+print(f'Serving on {server.server_address}')
+server.serve_forever()
